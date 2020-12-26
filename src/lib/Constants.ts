9a777a8df4dc40ba6881f3eb4ff6c32142d8db3a
T3NED <t3ned.root@gmail.com>
2020-12-26 03:06:14 +0000

    ✨ Configure transports

--- src/lib/Constants.ts
+++ src/lib/Constants.ts
@@ -1,3 +1,21 @@
+import { format } from "winston";
+const { printf, colorize } = format;
+
 export const defaultTimestamp = "DD/MM/YYYY|HH:mm:ss";
 export const defaultInfoFileName = "info.log";
 export const defaultErrorFileName = "error.log";
+
+export const levels = {
+    error: 0,
+    warn: 1,
+    info: 2,
+    verbose: 3,
+    debug: 4
+}
+
+export const consoleFormat = printf(({ message, level, timestamp, source }) => {
+    const colorizedTimestamp = colorize().colorize("timestamp", timestamp);
+    const colorizedSource = colorize().colorize("source", source);
+    const colorizedMessage = colorize().colorize(level, message)
+    return `[${colorizedTimestamp}] ${colorizedSource} | ${colorizedMessage}`;
+});
--- src/lib/Logger.ts
+++ src/lib/Logger.ts
@@ -1,4 +1,10 @@
-import { defaultTimestamp, defaultInfoFileName, defaultErrorFileName } from "./Constants";
+import { transports, transport, format, Logger as Winston, createLogger } from "winston";
+import { join } from "path";
+import { defaultTimestamp, defaultInfoFileName, defaultErrorFileName,
+    consoleFormat, levels } from "./Constants";
+
+const { Console, File } = transports;
+const { combine, timestamp, json } = format;
 
 export class Logger {
     public logFileDirectory: string;
@@ -9,6 +15,8 @@ export class Logger {
     public enableInfoLogs: boolean;
     public enableErrorLogs: boolean;
 
+    #logger: Winston;
+
     public constructor(logFileDirectory: string, options: Partial<LoggerOptions>) {
         this.logFileDirectory = logFileDirectory;
         this.timestamp = options.timestamp ?? defaultTimestamp;
@@ -17,8 +25,48 @@ export class Logger {
         this.enableConsoleLogs = options.enableConsoleLogs ?? true;
         this.enableInfoLogs = options.enableInfoLogs ?? true;
         this.enableErrorLogs = options.enableErrorLogs ?? true;
+
+        this.#logger = createLogger({
+            transports: this.transports,
+            levels
+        });
+    }
+
+    private get transports() {
+        const { timestampFormat, logFileDirectory,
+            infoLogFileName, errorLogFileName,
+            enableConsoleLogs, enableInfoLogs,
+            enableErrorLogs } = this;
+
+        const ConsoleTransport = new Console({
+            format: combine(timestampFormat, consoleFormat),
+            level: "debug"
+        });
+
+        const InfoTransport = new File({
+            filename: join(logFileDirectory, infoLogFileName),
+            format: combine(timestampFormat, json()),
+            level: "verbose"
+        });
+
+        const ErrorTransport = new File({
+            filename: join(logFileDirectory, errorLogFileName),
+            format: combine(timestampFormat, json()),
+            level: "warn"
+        });
+
+        return [
+            enableConsoleLogs ? ConsoleTransport : null,
+            enableInfoLogs ? InfoTransport : null,
+            enableErrorLogs ? ErrorTransport : null
+        ].filter(Boolean) as transport[];
+    }
+
+    private get timestampFormat() {
+        return timestamp({ format: this.timestamp });
     }
 }
+
 
 export interface LoggerOptions {
     timestamp: string;
