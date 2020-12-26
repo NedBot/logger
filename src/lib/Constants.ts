3d5fe7318dba58f0e4dbc89871194b294f7fe136
T3NED <t3ned.root@gmail.com>
2020-12-26 02:19:05 +0000

    ✨ Add config options

---
+++ src/lib/Constants.ts
@@ -0,0 +1,3 @@
+export const defaultTimestamp = "DD/MM/YYYY|HH:mm:ss";
+export const defaultInfoFileName = "info.log";
+export const defaultErrorFileName = "error.log";
--- src/lib/Logger.ts
+++ src/lib/Logger.ts
@@ -1,3 +1,30 @@
-export class Logger {
+import { defaultTimestamp, defaultInfoFileName, defaultErrorFileName } from "./Constants";
 
+export class Logger {
+    public logFileDirectory: string;
+    public timestamp: string;
+    public infoLogFileName: string;
+    public errorLogFileName: string;
+    public enableConsoleLogs: boolean;
+    public enableInfoLogs: boolean;
+    public enableErrorLogs: boolean;
+
+    public constructor(logFileDirectory: string, options: Partial<LoggerOptions>) {
+        this.logFileDirectory = logFileDirectory;
+        this.timestamp = options.timestamp ?? defaultTimestamp;
+        this.infoLogFileName = options.infoLogFileName ?? defaultInfoFileName;
+        this.errorLogFileName = options.errorLogFileName ?? defaultErrorFileName;
+        this.enableConsoleLogs = options.enableConsoleLogs ?? true;
+        this.enableInfoLogs = options.enableInfoLogs ?? true;
+        this.enableErrorLogs = options.enableErrorLogs ?? true;
+    }
+}
+
+export interface LoggerOptions {
+    timestamp: string;
+    infoLogFileName: string;
+    errorLogFileName: string;
+    enableConsoleLogs: boolean;
+    enableInfoLogs: boolean;
+    enableErrorLogs: boolean;
 }
