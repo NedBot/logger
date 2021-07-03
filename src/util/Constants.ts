9318b3cb4b39aa73252f9a71b6f07735bb8feb3c
T3NED <software@t3ned.com>
2021-07-03 22:24:19 +0100

    ✨ Add new constants file

---
+++ src/util/Constants.ts
@@ -0,0 +1,99 @@
+import { LoggerOptions } from "../lib/Logger";
+import { join } from "path";
+
+export enum LogLevelSeverity {
+  ERROR,
+  WARN,
+  LOG,
+  INFO,
+  VERBOSE,
+  DEBUG
+}
+
+export enum LogLevel {
+  ERROR = "error",
+  WARN = "warn",
+  LOG = "corelog",
+  INFO = "info",
+  VERBOSE = "verbose",
+  DEBUG = "debug"
+}
+
+export const LogLevels = {
+  [LogLevel.ERROR]: LogLevelSeverity.ERROR,
+  [LogLevel.WARN]: LogLevelSeverity.WARN,
+  [LogLevel.LOG]: LogLevelSeverity.LOG,
+  [LogLevel.INFO]: LogLevelSeverity.INFO,
+  [LogLevel.VERBOSE]: LogLevelSeverity.VERBOSE,
+  [LogLevel.DEBUG]: LogLevelSeverity.DEBUG
+};
+
+export type FontForegroundColor =
+  | "black"
+  | "red"
+  | "green"
+  | "yellow"
+  | "blue"
+  | "magenta"
+  | "cyan"
+  | "white"
+  | "gray"
+  | "grey";
+
+export type FontBackgroundColor =
+  | "blackBG"
+  | "redBG"
+  | "greenBG"
+  | "yellowBG"
+  | "blueBG"
+  | "magentaBG"
+  | "cyanBG"
+  | "whiteBG";
+
+export type FontStyle =
+  | "bold"
+  | "dim"
+  | "italic"
+  | "underline"
+  | "inverse"
+  | "hidden"
+  | "strikethrough";
+
+export type LoggerColor =
+  | `${FontStyle} ${FontForegroundColor} ${FontBackgroundColor}`
+  | `${FontStyle} ${FontForegroundColor}`
+  | `${FontStyle} ${FontBackgroundColor}`
+  | `${FontForegroundColor} ${FontBackgroundColor}`
+  | FontStyle
+  | FontForegroundColor
+  | FontBackgroundColor;
+
+type DefaultOptions = Pick<
+  LoggerOptions,
+  | "logFilesDirectory"
+  | "mainLogFileName"
+  | "errorLogFileName"
+  | "timestampFormat"
+  | "colors"
+>;
+
+export const Defaults: DefaultOptions = {
+  logFilesDirectory: join(process.cwd(), "logs"),
+  mainLogFileName: "info.log",
+  errorLogFileName: "error.log",
+  timestampFormat: "DD/MM/YYYY @ HH:mm:ss",
+  colors: {
+    timestamp: "bold grey",
+    source: "bold magenta",
+    error: "red",
+    warn: "yellow",
+    info: "blue",
+    verbose: "green",
+    debug: "grey",
+    errorBG: "bold red",
+    warnBG: "bold yellow",
+    infoBG: "bold blue",
+    verboseBG: "bold green",
+    debugBG: "bold grey"
+  }
+};
