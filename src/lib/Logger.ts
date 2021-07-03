6f874ced2e348611c6298acd40798868af879b45
T3NED <software@t3ned.com>
2021-07-03 22:24:32 +0100

    ♻️ Refactor logger class

--- src/lib/Logger.ts
+++ src/lib/Logger.ts
@@ -1,106 +1,234 @@
-import { transports, transport, format, Logger as Winston, createLogger, addColors } from "winston";
+import Winston, { format, transport, transports } from "winston";
+import { LogLevels, LogLevel, LoggerColor, Defaults } from "../util/Constants";
+import { inspect } from "util";
 import { join } from "path";
-import { defaultTimestamp, defaultInfoFileName, defaultErrorFileName,
-    consoleFormat, levels, colors, defaultLogDirectory } from "./Constants";
 
-const { Console, File } = transports;
-const { combine, timestamp, json } = format;
+const colorizer = format.colorize();
 
 export class Logger {
-    public logFileDirectory: string;
-    public timestamp: string;
-    public infoLogFileName: string;
-    public errorLogFileName: string;
-    public enableConsoleLogs: boolean;
-    public enableInfoLogs: boolean;
-    public enableErrorLogs: boolean;
+  /**
+   * The directory that holds the log files.
+   */
+  public logFilesDirectory: string;
 
-    #logger: Winston;
+  /**
+   * The name of the main log file.
+   */
+  public mainLogFileName: string;
 
-    public constructor(options: Partial<LoggerOptions>) {
-        addColors(colors);
+  /**
+   * The name of the error log file.
+   */
+  public errorLogFileName: string;
 
-        this.logFileDirectory = options.logFileDirectory ?? defaultLogDirectory;
-        this.timestamp = options.timestamp ?? defaultTimestamp;
-        this.infoLogFileName = options.infoLogFileName ?? defaultInfoFileName;
-        this.errorLogFileName = options.errorLogFileName ?? defaultErrorFileName;
-        this.enableConsoleLogs = options.enableConsoleLogs ?? true;
-        this.enableInfoLogs = options.enableInfoLogs ?? true;
-        this.enableErrorLogs = options.enableErrorLogs ?? true;
+  /**
+   * Whether or not console logs are enabled.
+   */
+  public enableConsoleLogs: boolean;
 
-        if (!this.enableConsoleLogs && !this.enableInfoLogs && !this.enableErrorLogs) {
-            throw new TypeError("Please enable at least 1 transport");
-        }
+  /**
+   * Whether or not main file logs are enabled.
+   */
+  public enableMainLogFile: boolean;
 
-        this.#logger = createLogger({
-            transports: this.transports,
-            levels
-        });
+  /**
+   * Whether or not error file logs are enabled.
+   */
+  public enableErrorLogFile: boolean;
+
+  /**
+   * The format of the log timestamp.
+   */
+  public timestampFormat: string;
+
+  /**
+   * The source name.
+   */
+  public source?: string;
+
+  /**
+   * The winston logger.
+   */
+  #winston: Winston.Logger;
+
+  /**
+   * @param options The logger options.
+   */
+  public constructor(options: Partial<LoggerOptions>) {
+    this.logFilesDirectory = options.logFilesDirectory ?? Defaults.logFilesDirectory;
+    this.mainLogFileName = options.mainLogFileName ?? Defaults.mainLogFileName;
+    this.errorLogFileName = options.errorLogFileName ?? Defaults.errorLogFileName;
+    this.timestampFormat = options.timestampFormat ?? Defaults.timestampFormat;
+    this.enableConsoleLogs = options.enableConsoleLogs ?? true;
+    this.enableMainLogFile = options.enableMainLogFile ?? false;
+    this.enableErrorLogFile = options.enableErrorLogFile ?? false;
+    this.source = options.source;
+
+    if (!this.enableConsoleLogs && !this.enableMainLogFile && !this.enableErrorLogFile) {
+      throw new Error("You must enable at least one transport.");
     }
 
-    public info(source: string, message: string, ...meta: any[]) {
-        this.#logger.info(message, { source, meta });
+    Winston.addColors(options.colors || Defaults.colors);
+
+    this.#winston = Winston.createLogger({
+      transports: this.createTransports(),
+      levels: LogLevels
+    });
+  }
+
+  /**
+   * Prints a line using the info level.
+   * @param message The message to log
+   * @param meta The meta data
+   */
+  public log(message: any, ...meta: any[]) {
+    this.info(message, ...meta);
+  }
+
+  /**
+   * Prints a line using the info level.
+   * @param message The message to log
+   * @param meta The meta data
+   */
+  public info(message: any, ...meta: any[]) {
+    this.#winston.info(this.inspect(message), { meta });
+  }
+
+  /**
+   * Prints a line using the error level.
+   * @param error The error to log
+   * @param meta The meta data
+   */
+  public error(error: Error | string, ...meta: any[]) {
+    const stack = error instanceof Error ? error.stack || error.message : error;
+    const message = error instanceof Error ? error.message : error;
+    this.#winston.error(message, { meta, stack });
+  }
+
+  /**
+   * Prints a line using the warn level.
+   * @param message The message to log
+   * @param meta The meta data
+   */
+  public warn(message: string, ...meta: any[]) {
+    this.#winston.warn(this.inspect(message), { meta });
+  }
+
+  /**
+   * Prints a line using the verbose level.
+   * @param message The message to log
+   * @param meta The meta data
+   */
+  public verbose(message: string, ...meta: any[]) {
+    this.#winston.verbose(this.inspect(message), { meta });
+  }
+
+  /**
+   * Prints a line using the debug level.
+   * @param message The message to log
+   * @param meta The meta data
+   */
+  public debug(message: string, ...meta: any[]) {
+    this.#winston.debug(this.inspect(message), { meta });
+  }
+
+  /**
+   * Inspects a message.
+   * @param message The message to inspect
+   * @returns The inspected message
+   */
+  private inspect(message: any) {
+    if (typeof message === "string") return message;
+    return inspect(message, { depth: 1 });
+  }
+
+  /**
+   * Creates the transports for the logger.
+   * @returns The transports
+   */
+  private createTransports(): transport[] {
+    const timestampFormat = this.createTimestampFormat();
+    const consoleFormat = this.createConsoleFormat();
+
+    const loggerTransports: transport[] = [];
+
+    if (this.enableConsoleLogs) {
+      const consoleTransport = new transports.Console({
+        format: format.combine(timestampFormat, consoleFormat),
+        level: LogLevel.DEBUG
+      });
+
+      loggerTransports.push(consoleTransport);
     }
 
-    public error(source: string, error: Error | string, ...meta: any[]) {
-        const stack = error instanceof Error ? error.stack || error.message : error;
-        const message = error instanceof Error ? error.message : error;
-        this.#logger.error(message, { source, stack, meta });
+    if (this.enableMainLogFile) {
+      const mainFileTransport = new transports.File({
+        filename: join(this.logFilesDirectory, this.mainLogFileName),
+        format: format.combine(timestampFormat, format.json()),
+        level: LogLevel.VERBOSE
+      });
+
+      loggerTransports.push(mainFileTransport);
     }
 
-    public warn(source: string, message: string, ...meta: any[]) {
-        this.#logger.warn(message, { source, meta });
+    if (this.enableErrorLogFile) {
+      const errorFileTransport = new transports.File({
+        filename: join(this.logFilesDirectory, this.errorLogFileName),
+        format: format.combine(timestampFormat, format.json()),
+        level: LogLevel.WARN
+      });
+
+      loggerTransports.push(errorFileTransport);
     }
 
-    public verbose(source: string, message: string, ...meta: any[]) {
-        this.#logger.verbose(message, { source, meta });
-    }
+    return loggerTransports;
+  }
 
-    public debug(source: string, message: string, ...meta: any[]) {
-        this.#logger.debug(message, { source, meta });
-    }
+  /**
+   * Creates a timestamp format.
+   * @returns The timestamp format
+   */
+  private createTimestampFormat() {
+    return format.timestamp({ format: this.timestampFormat });
+  }
 
-    private get transports() {
-        const { timestampFormat, logFileDirectory,
-            infoLogFileName, errorLogFileName,
-            enableConsoleLogs, enableInfoLogs,
-            enableErrorLogs } = this;
+  /**
+   * Creates a console format.
+   * @returns The console format
+   */
+  private createConsoleFormat() {
+    return format.printf(({ level, message, timestamp }) => {
+      let consoleFormat = "";
 
-        const ConsoleTransport = new Console({
-            format: combine(timestampFormat, consoleFormat),
-            level: "debug"
-        });
+      const colorizedTimestamp = colorizer.colorize("timestamp", `[${timestamp}]`);
+      consoleFormat += `${colorizedTimestamp} `;
 
-        const InfoTransport = new File({
-            filename: join(logFileDirectory, infoLogFileName),
-            format: combine(timestampFormat, json()),
-            level: "verbose"
-        });
+      if (this.source) {
+        const colorizedSource = colorizer.colorize("source", this.source);
+        consoleFormat += `${colorizedSource} `;
+      }
 
-        const ErrorTransport = new File({
-            filename: join(logFileDirectory, errorLogFileName),
-            format: combine(timestampFormat, json()),
-            level: "warn"
-        });
+      const colorizedLevel = colorizer.colorize(`${level}BG`, level);
+      consoleFormat += `(${colorizedLevel}): `;
 
-        return [
-            enableConsoleLogs ? ConsoleTransport : null,
-            enableInfoLogs ? InfoTransport : null,
-            enableErrorLogs ? ErrorTransport : null
-        ].filter(Boolean) as transport[];
-    }
+      const colorizedMessage = colorizer.colorize(level, message);
+      consoleFormat += colorizedMessage;
 
-    private get timestampFormat() {
-        return timestamp({ format: this.timestamp });
-    }
+      return consoleFormat;
+    });
+  }
 }
 
 export interface LoggerOptions {
-    logFileDirectory: string;
-    timestamp: string;
-    infoLogFileName: string;
-    errorLogFileName: string;
-    enableConsoleLogs: boolean;
-    enableInfoLogs: boolean;
-    enableErrorLogs: boolean;
+  logFilesDirectory: string;
+  mainLogFileName: string;
+  errorLogFileName: string;
+  timestampFormat: string;
+  enableConsoleLogs: boolean;
+  enableMainLogFile: boolean;
+  enableErrorLogFile: boolean;
+  source: string;
+  colors: LoggerColors;
 }
+
+export type LoggerColors = Record<string, LoggerColor>;
